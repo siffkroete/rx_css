2a4575705e494ef0f326f16962db05594ab48bb8
siffkroete <siffkroete@gmail.com>
2020-07-12 16:38:53 +0200

    Webpack mit Babel, css, source-map-loader und rxjs, build und start funktionieren

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const Rx = require('rxjs');
-import { fromEvent} from 'rxjs';
-import { map } from 'rxjs';
+import { BehaviorSubject, Observable, of, fromEvent } from 'rxjs';
+import { buffer, bufferCount, expand, filter, map,  share, tap, withLatestFrom } from 'rxjs/operators';
 import RxCSS from 'rxcss';
+
 import "./css/index.css";
 
 
 (function start(input) {
 
-    const mouse$ = fromEvent(document, 'mousemove');
-    mouse$.map(({ clientX, clientY }) => ({
-        x: clientX,
-        y: clientY
-    }));
+    const mouse$ = fromEvent(document, 'mousemove')
+    .pipe(
+        map( (clientX, clientY) => ({
+            x: clientX,
+            y: clientY
+        }))
+    );
 
-
-    const style$ = RxCSS({
-        mouse: mouse$,
-    });
-
+    // const style$ = RxCSS({
+    //     mouse: mouse$,
+    // });
 
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,25 @@ module.exports = {
         contentBase: './build'
     },
     
-    mode: 'development',
+    mode: 'development', // TODO : mode: 'production',
     module: {
       rules: [
         {
           test: /\.css$/i,
+          exclude: /node_modules/,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          use: ['source-map-loader'],
         },
       ],
     }
