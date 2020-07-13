6ab34e4ebb5a67454310ac3fe571c53d3b00c524
siffkroete <siffkroete@gmail.com>
2020-07-13 19:28:21 +0200

    Drag And Drop finish!!!

--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
 import { BehaviorSubject, Observable, of, fromEvent } from 'rxjs';
-import { buffer, bufferCount, expand, filter, map,  share, tap, withLatestFrom } from 'rxjs/operators';
-import RxCSS from 'rxcss';
+import { buffer, bufferCount, expand, filter, map, switchMap, takeUntil, share, tap, withLatestFrom } from 'rxjs/operators';
 import styledash from "./styledash.js";
 import "./css/index.css";
+import { animationFrameScheduler } from 'rxjs';
+import { interval } from 'rxjs';
+
 
 
 (function start(input) {
     const myContent = document.getElementById('myContent');
     if(!myContent) throw new Exception('Kein Content!!!');
     const infoDiv = document.getElementById('info');
-    const mouse$ = fromEvent(document, 'mousemove')
-    .pipe(
-        map( (mouseEvent) => ({
-            x: mouseEvent.clientX,
-            y: mouseEvent.clientY,
-            which: mouseEvent.which
-        }))
+
+    var oldTimeStamp = 0;
+
+
+    const mousedown$ = fromEvent(document, 'mousedown');
+    const mousemove$ = fromEvent(document, 'mousemove');
+    const mouseup$ = fromEvent(document, 'mouseup');
+
+
+    const drag$ = mousedown$.pipe(
+        switchMap(
+          (start) => {
+              console.log('switchMap(..) start..');
+            return mousemove$.pipe(map(move => {
+              move.preventDefault();
+              return {
+                x: move.clientX - start.offsetX,
+                y: move.clientY - start.offsetY
+              }
+            }),
+            tap((coord) => {
+                styledash(myContent).set('mouse-x', coord.x);
+                styledash(myContent).set('mouse-y', coord.y);
+            }),
+            takeUntil(mouseup$));
+          }
+        )
     );
 
-    mouse$.subscribe((ev) => {
-        console.log(ev.which);
-        if(ev.which == 1) {
-            styledash(myContent).set('mouse-x', ev.x);
-            styledash(myContent).set('mouse-y', ev.y);
-        }
-    });
-
+          drag$.subscribe(() => { console.log('Hie')})
     
-    
-
-
 })();
