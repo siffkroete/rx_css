81baae12a0c7cc8e1cca64f786ab70a7136b9164
siffkroete <siffkroete@gmail.com>
2020-07-13 20:29:04 +0200

    Koordinaten erst unten bei subscribe an css senden/ändern

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { BehaviorSubject, Observable, of, fromEvent } from 'rxjs';
-import { buffer, bufferCount, expand, filter, map, switchMap, takeUntil, share, tap, withLatestFrom } from 'rxjs/operators';
+import { buffer, bufferCount, expand, filter, map, switchMap, takeUntil, take,share, tap, withLatestFrom } from 'rxjs/operators';
 import styledash from "./styledash.js";
 import "./css/index.css";
 import { animationFrameScheduler } from 'rxjs';
@@ -19,11 +19,10 @@ import { interval } from 'rxjs';
     const mousemove$ = fromEvent(document, 'mousemove');
     const mouseup$ = fromEvent(document, 'mouseup');
 
-
     const drag$ = mousedown$.pipe(
         switchMap(
           (start) => {
-              console.log('switchMap(..) start..');
+            console.log('switchMap(..) start:', start);
             return mousemove$.pipe(map(move => {
               move.preventDefault();
               return {
@@ -31,15 +30,14 @@ import { interval } from 'rxjs';
                 y: move.clientY - start.offsetY
               }
             }),
-            tap((coord) => {
-                styledash(myContent).set('mouse-x', coord.x);
-                styledash(myContent).set('mouse-y', coord.y);
-            }),
             takeUntil(mouseup$));
           }
         )
     );
 
-          drag$.subscribe(() => { console.log('Hie')})
+    drag$.subscribe((ev) => { 
+        styledash(myContent).set('mouse-x', ev.x);
+        styledash(myContent).set('mouse-y', ev.y);
+    })
     
 })();
